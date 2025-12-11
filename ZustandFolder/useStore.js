8889cfa676e820cb8db8c5661e99e39247e5bb41
sakshamOrdex <saksham.ojha.ordex@gmail.com>
2025-12-11 16:01:06 +0530

    Code Added

--- ZustandFolder/useStore.js
+++ ZustandFolder/useStore.js
@@ -5,6 +5,7 @@ const countStore = create((set) => {
     increment: () => set((state) => ({ count: state.count + 1 }))
     decrement: () => set((state) => ({ count: state.count - 1 }))
     reset: () => set((state) => ({ count: 0 }))
+    
 })
 
 export default countStore
